bdcd831b75c448b24bd70da271d416cb71c07afd
RTymch <48328192+RTymch@users.noreply.github.com>
2019-03-09 16:10:12 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -22,13 +22,19 @@ import java.util.Scanner;
 
 public class Main {
     
-    public static void main(String[] args) {
-	int balance =100;
-  System.out.println("Програма \"Банкомат\"");
+    public void mainMenu(){
+    System.out.println("Програма \"Банкомат\"");
   	System.out.println("Зробіть вибір, натиснувши відповідну цифру");
 	System.out.println("Перевірити баланс рахунку - 1");
 	System.out.println("Зняти гроші - 2");
 	System.out.println("Змінити пінкод - 3");
+    }
+    
+    public static void main(String[] args) {
+	int balance =100;
+       Main m= new Main();
+        m.mainMenu();
+  
   Scanner myVar = new Scanner(System.in);
                int vvid = myVar.nextInt();
   System.out.println("Ви вибрали: " + vvid);      
@@ -37,7 +43,12 @@ public class Main {
         if(vvid==1){
             System.out.println("Баланс рахунку : " + balance);
         }
-        
-        
+          System.out.println(" ");     
+        System.out.println("Повернутися до вибору? - Натисніть 1");
+        Scanner myVar2= new Scanner(System.in);
+               int vvid2= myVar2.nextInt();
+        if(vvid2==1){
+            m.mainMenu();
+        }
 	}
 }
